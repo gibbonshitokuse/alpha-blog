99c9a9bfb849274d104497bd4eac86e32c42f67b
Lee Gibbons <gibbons@hitokuse.com>
2017-04-19 06:11:31 +0000

    create new template, action and route

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419012222) do
+ActiveRecord::Schema.define(version: 20170419033312) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
