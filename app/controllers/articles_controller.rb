603d047e0b2fcc76da4519f0e108bff0ea162dc9
Lee Gibbons <gibbons@hitokuse.com>
2017-04-20 05:50:15 +0000

    Add partials adn destory action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,49 @@
 class ArticlesController < ApplicationController
+   
+    
     def new
         @article = Article.new
+    end
+    
+    def index
+        @articles = Article.all
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+        
     end
     
     def create
         @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was successfully created"
-            redirect_to article_path(@article)
+            redirect_to article_path(@article) and return
         else
             render 'new'
         end
     end
     
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        
+        end
+    end
+    
     def show
-       @article = Article.find(params[:id]) 
+       @article = Article.find(params[:id])
+    end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was successfully deleted"
+        redirect_to articles_path
     end
     
     private
